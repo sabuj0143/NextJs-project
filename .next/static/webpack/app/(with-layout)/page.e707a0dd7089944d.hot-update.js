a4561377b793e8411c11aa7c4ea3398fdfd96c87
sabuj0143 <sabujhowlader011@gmai.com>
2023-06-21 01:22:22 +0600

    Image system added

---
+++ .next/static/webpack/app/(with-layout)/page.e707a0dd7089944d.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/(with-layout)/page",{
+
+/***/ "(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5Cprojects%5Ctry-next-js%5Csrc%5Cassets%5Cslider-5.jpg&modules=C%3A%5Cprojects%5Ctry-next-js%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Cimage.js&server=false!":
+/*!******************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5Cprojects%5Ctry-next-js%5Csrc%5Cassets%5Cslider-5.jpg&modules=C%3A%5Cprojects%5Ctry-next-js%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Cimage.js&server=false! ***!
+  \******************************************************************************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./src/assets/slider-5.jpg */ \"(app-client)/./src/assets/slider-5.jpg\"));\nPromise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./node_modules/next/dist/client/image.js */ \"(app-client)/./node_modules/next/dist/client/image.js\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1jbGllbnQpLy4vbm9kZV9tb2R1bGVzL25leHQvZGlzdC9idWlsZC93ZWJwYWNrL2xvYWRlcnMvbmV4dC1mbGlnaHQtY2xpZW50LWVudHJ5LWxvYWRlci5qcz9tb2R1bGVzPUMlM0ElNUNwcm9qZWN0cyU1Q3RyeS1uZXh0LWpzJTVDc3JjJTVDYXNzZXRzJTVDc2xpZGVyLTUuanBnJm1vZHVsZXM9QyUzQSU1Q3Byb2plY3RzJTVDdHJ5LW5leHQtanMlNUNub2RlX21vZHVsZXMlNUNuZXh0JTVDZGlzdCU1Q2NsaWVudCU1Q2ltYWdlLmpzJnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEscUtBQXlGO0FBQ3pGIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ZWFmYiJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQoLyogd2VicGFja01vZGU6IFwiZWFnZXJcIiAqLyBcIkM6XFxcXHByb2plY3RzXFxcXHRyeS1uZXh0LWpzXFxcXHNyY1xcXFxhc3NldHNcXFxcc2xpZGVyLTUuanBnXCIpO1xuaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImVhZ2VyXCIgKi8gXCJDOlxcXFxwcm9qZWN0c1xcXFx0cnktbmV4dC1qc1xcXFxub2RlX21vZHVsZXNcXFxcbmV4dFxcXFxkaXN0XFxcXGNsaWVudFxcXFxpbWFnZS5qc1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(app-client)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5Cprojects%5Ctry-next-js%5Csrc%5Cassets%5Cslider-5.jpg&modules=C%3A%5Cprojects%5Ctry-next-js%5Cnode_modules%5Cnext%5Cdist%5Cclient%5Cimage.js&server=false!\n"));
+
+/***/ })
+
+});
--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import img1 from '../../assets/slider-5.jpg';
+// import img2 from '../../assets/slider-6.jpg';
+import Image from 'next/image';
 
- const  HomePage = () => {
+
+const HomePage = () => {
   return (
     <main>
       <h2>Home</h2>
+      <Image src={img1} alt="" />
     </main>
   )
 }
